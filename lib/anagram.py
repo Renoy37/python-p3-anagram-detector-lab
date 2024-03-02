2541b79edbafc0516f9639af461570ee0014a88c
Robins <robinmgk077@gmail.com>
2024-03-02 11:12:44 +0300

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, new_word):
+        return [word for word in new_word if sorted(self.word) == sorted(word.lower())]
